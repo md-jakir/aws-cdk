e6d8996246224358c19a2e1f4ddc7ddd4a1a81c6
md-jakir <mdavir814@gmail.com>
2025-12-06 17:00:17 +0600

    fixed validation and unit test error

--- tests/unit/test_hello_ecs_stack.py
+++ tests/unit/test_hello_ecs_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from hello_ecs.hello_ecs_stack import HelloEcsStack
+from hello_ecs.hello_ecs_stack import DemoEcsStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in hello_ecs/hello_ecs_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = HelloEcsStack(app, "hello-ecs")
+    stack = DemoEcsStack(app, "hello-ecs")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
